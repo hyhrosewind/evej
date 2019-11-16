7ee340b5742e749d0c21b10cab127e679377fe39
hyh12261998 <1790529422@qq.com>
2019-11-16 23:20:13 +0800

    基本架构 1

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+// import Vue from 'vue'
+// import Router from 'vue-router'
+
+// // const Home = () => import('views/home/Home')
+
+// Vue.use(Router)
+
+// const routes = [
+//   {
+//     path: '',
+//     redirect: '/home'
+//   },
+//   {
+//     path: '/home',
+//     component: Home
+//   }
+// ]
+
+// export default new Router({
+//   routes,
+//   mode: history
+// })
