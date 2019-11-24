5ee029a14cee0b8c6203b499a8d8cee9e5b1ae1b
hyh12261998 <1790529422@qq.com>
2019-11-24 17:27:17 +0800

    update

---
+++ src/views/dspatialdata/EchartsScatter.vue
@@ -0,0 +1,27 @@
+<!--  三维数据 散点图  -->
+<template>
+  <div class="echartInfo">
+    <scatter/>
+  </div>
+</template>
+
+<script>
+
+import scatter from 'components/common/echarts/dspatialdata/scatter'
+
+export default {
+  name: 'EchartsScatter',
+  components: {
+    scatter
+  }
+}
+</script>
+
+<style scoped>
+.echartInfo {
+  margin: 20px;
+  padding: 20px;
+  height: 540px;
+  text-align: center;
+}
+</style>
